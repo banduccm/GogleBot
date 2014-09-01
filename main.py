bbd6c279900eb2e93d0983a19be9d789eebdf313
banduccm <poco153@gmail.com>
2014-09-01 10:55:23 -0400

    Parsing
    - Started learning how to parse:
    - Added the ability to find 'names' that start with an '@'
    - Added the ability to find 'scores' in messages with names that start
    with + or -
    - Removed notifier calls that were causing warnings at runtime

--- main.py
+++ main.py
@@ -2,13 +2,69 @@ from tornado import ioloop
 import hangups
 from hangups.notify import Notifier
 
-class chat(object):
+class MessageParser(object):
+    def findNameInMessage(msgString):
+        returnString = ''
+        nameEnd = 0
+        nameStart = -1
 
+        if '@' in msgString:
+            nameStart = msgString.find('@')
+            
+        nameStart += 1
+
+        if nameStart > 0:
+            nameEnd = msgString.find(' ', nameStart)
+            
+            if (nameEnd < 0):
+                returnString =  msgString[nameStart:]
+            else:
+                returnString =  msgString[nameStart:nameEnd]
+
+        return returnString
+
+    def findScoreInMessage(msgString):
+        returnString = ''
+        scoreEnd = 0
+        scoreStart = -1
+        scoreSign = ''
+
+        if '+' in msgString:
+            scoreSign = '+'
+            scoreStart = msgString.find('+')
+        elif '-' in msgString:
+            scoreSign = '-'
+            scoreStart = msgString.find('-')
+
+        scoreStart += 1
+
+        if scoreStart > 0:
+            scoreEnd = msgString.find(' ', scoreStart)
+
+            if (scoreEnd < 0):
+                returnString = scoreSign + msgString[scoreStart:]
+            else:
+                returnString = scoreSign + msgString[scoreStart:scoreEnd]
+
+        return returnString
+        
+        
+    def parseMessage(chat_message):
+        returnString = ''
+        name = MessageParser.findNameInMessage(chat_message.text)
+        score = MessageParser.findScoreInMessage(chat_message.text)
+        if name:
+            if score:
+                returnString = name + ' : ' + score
+            
+        return returnString
+    
+class ChatClient(object):
     def __init__(self):
         try:
             cookies = hangups.get_auth_stdin('c.txt')
         except hangups.GoogleAuthError as e:
-            print('Login failer ({})'.format(e))
+            print('Login failed ({})'.format(e))
             sys.exit(1)
         
         self._client = hangups.Client(cookies)
@@ -22,20 +78,22 @@ class chat(object):
     def _on_connect(self):
         self._conv_list = hangups.ConversationList(self._client)
         self._user_list = hangups.UserList(self._client)
-        self._notifier = Notifier(self._client, self._conv_list)
 
     def _on_message(self, chat_message):
         conv = hangups.ConversationList.get(self._conv_list, chat_message.conv_id)
         if chat_message.user_id != self._client.self_user_id:
-            """conv.send_message(text=chat_message.text)"""
-            print(chat_message)
+            #Message Handling Code
+            msgstr= MessageParser.parseMessage(chat_message)
+            if msgstr:
+                conv.send_message(text=msgstr)
+            print("message received")
 
     def _on_disconnect(self):
         print('Disconnected')
 
 def main():
     try:
-        chat()
+        ChatClient()
     except KeyboardInterrupt:
         pass
     except:
